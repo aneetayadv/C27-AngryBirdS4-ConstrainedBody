bdcf384be07cc2b0d0fdbd33ab13929eefd9dec4
aneetayadv <anitayadav21@gmail.com>
2020-10-01 08:40:07 +0530

    Adding modified files Aneeta Yadv

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
+var log1,log2,log3,lo4,log5,log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,12 +38,15 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80,PI/2);
 
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
@@ -58,7 +64,11 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+
+    fill("yellow");
+    text(mouseX + "," + mouseY,mouseX,mouseY);
 }
